34251d8fa7fb1e0cc88d9a82407b0bd912c303ac
Hafidz <halfpheez92@gmail.com>
2018-11-17 01:59:34 +0800

    implemented React dom elements and output elements.

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const tasks = ['Call Inma', 'Meet Inma', 'Kiss Inma'];
+
+const element = React.createElement('ol', null,
+  tasks.map((task, index) => React.createElement('li', {key: index}, task))
+);
+
+ReactDOM.render(element, document.getElementById('root'));
