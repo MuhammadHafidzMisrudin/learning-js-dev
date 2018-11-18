ccc7776b406c7e380205b9c478748ceba2df972b
Hafidz <halfpheez92@gmail.com>
2018-11-18 18:27:54 +0800

    implemented Components for Compositional Model in React.

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 import ReactDOM from 'react-dom';
 
 const tasks = ['Call Inma', 'Meet Inma', 'Give Inma A Kiss'];
@@ -7,13 +8,44 @@ const tasks = ['Call Inma', 'Meet Inma', 'Give Inma A Kiss'];
 //   tasks.map((task, index) => React.createElement('li', {key: index}, task))
 // );
 
-const element = (
-  <div>
-    <h1>My Tasks</h1>
-    <ol>
-      { tasks.map((task, index) => <li key={index}>{task}</li>) }
-    </ol>
-  </div>
-)
+// const element = (
+//   <div>
+//     <h1>My Tasks</h1>
+//     <ol>
+//       { tasks.map((task, index) => <li key={index}>{task}</li>) }
+//     </ol>
+//   </div>
+// )
 
-ReactDOM.render(element, document.getElementById('root'));
+class Title extends Component {
+  render() {
+    return (
+      <h1>My Tasks</h1>
+    )
+  }
+}
+
+class List extends Component {
+  render() {
+
+    return (
+      <ol>
+        { tasks.map((task, index) => <li key={index}>{task}</li>) }
+      </ol>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Title/>
+        <List/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById('root'));
