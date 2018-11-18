22c70c4b82a55d362004f0d00da7f43dc7204cb4
Hafidz <halfpheez92@gmail.com>
2018-11-18 18:08:07 +0800

    implemented JSX to encapsulate dom elements in React.

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const tasks = ['Call Inma', 'Meet Inma', 'Kiss Inma'];
+const tasks = ['Call Inma', 'Meet Inma', 'Give Inma A Kiss'];
 
-const element = React.createElement('ol', null,
-  tasks.map((task, index) => React.createElement('li', {key: index}, task))
-);
+// const element = React.createElement('ol', null,
+//   tasks.map((task, index) => React.createElement('li', {key: index}, task))
+// );
+
+const element = (
+  <div>
+    <h1>My Tasks</h1>
+    <ol>
+      { tasks.map((task, index) => <li key={index}>{task}</li>) }
+    </ol>
+  </div>
+)
 
 ReactDOM.render(element, document.getElementById('root'));
