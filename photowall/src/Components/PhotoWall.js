68c50e7edc2061551e41af7f811835e6eb838551
Hafidz <halfpheez92@gmail.com>
2018-11-20 23:10:25 +0800

    modified implementations, added Components, Photowall container and Photo.

---
+++ photowall/src/Components/PhotoWall.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Photo from './Photo';
+
+class PhotoWall extends Component {
+  render() {
+
+    return (
+      <div>
+        {/*PhotoWall Component iterates through an array of Objects and it generates photos of Photo Component
+        or Photo Component instances with pass in props.*/}
+        {this.props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+      </div>);
+  }
+}
+
+export default PhotoWall;
