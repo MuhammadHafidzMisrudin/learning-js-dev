5e522fd632c6deadb58b85c2a294c6412f09a0ea
Hafidz <halfpheez92@gmail.com>
2018-12-04 23:37:40 +0800

    implemented a stateless functional components in react.

--- photowall/src/Components/PhotoWall.js
+++ photowall/src/Components/PhotoWall.js
@@ -2,16 +2,26 @@ import React, {Component} from 'react';
 
 import Photo from './Photo';
 
-class PhotoWall extends Component {
-  render() {
-
-    return (
-      <div className="photo-grid">
-        {/* PhotoWall Component iterates through an array of Objects and it generates photos of Photo Component
-        or Photo Component instances with pass in props. Each Photo instance has a <figure> element. */}
-        {this.props.posts.map((post, index) => <Photo key={index} post={post}/>)}
-      </div>);
-  }
+// Create a stateless function component of PhotoWall.
+function PhotoWall(props) {
+  return (
+    <div className="photo-grid">
+      {/* PhotoWall Component iterates through an array of Objects and it generates photos of Photo Component
+      or Photo Component instances with pass in props. Each Photo instance has a <figure> element. */}
+      {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+    </div>);
 }
+
+// class PhotoWall extends Component {
+//   render() {
+//
+//     return (
+//       <div className="photo-grid">
+//         {/* PhotoWall Component iterates through an array of Objects and it generates photos of Photo Component
+//         or Photo Component instances with pass in props. Each Photo instance has a <figure> element. */}
+//         {this.props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+//       </div>);
+//   }
+// }
 
 export default PhotoWall;
