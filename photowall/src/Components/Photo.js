54b0c5d6377ad94b99b1b296d3bdbb6eb886ba73
Hafidz <halfpheez92@gmail.com>
2018-12-09 23:24:31 +0800

    modified and implemented stateless functional components for Photo and Title components.

--- photowall/src/Components/Photo.js
+++ photowall/src/Components/Photo.js
@@ -1,22 +1,39 @@
 import React, {Component} from 'react';
 
-class Photo extends Component {
-  render() {
-    // Create post variable and set as Component instance that is passed in as props.
-    const post = this.props.post;
+// Create a stateless function component of Photo.
+function Photo(props) {
+  
+  // Create post variable and set as Component instance that is passed in as props.
+  const post = props.post;
+  return (
+    <figure className="figure">
+      <img className="photo" src={post.imageLink} alt={post.description}></img>
+      <figcaption>
+        <p>{post.description}</p>
+      </figcaption>
 
-    return (
-      <figure className="figure">
-        <img className="photo" src={post.imageLink} alt={post.description}></img>
-        <figcaption>
-          <p>{post.description}</p>
-        </figcaption>
-
-        <div className="button-container">
-          <button className="remove-button">Remove</button>
-        </div>
-      </figure>);
-  }
+      <div className="button-container">
+        <button className="remove-button">Remove</button>
+      </div>
+    </figure>);
 }
+
+// class Photo extends Component {
+//   render() {
+//     // Create post variable and set as Component instance that is passed in as props.
+//     const post = this.props.post;
+//     return (
+//       <figure className="figure">
+//         <img className="photo" src={post.imageLink} alt={post.description}></img>
+//         <figcaption>
+//           <p>{post.description}</p>
+//         </figcaption>
+//
+//         <div className="button-container">
+//           <button className="remove-button">Remove</button>
+//         </div>
+//       </figure>);
+//   }
+// }
 
 export default Photo;
--- photowall/src/Components/Title.js
+++ photowall/src/Components/Title.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react';
 
 import List from './List';
 
-class Title extends Component {
-  render() {
-    return (
-      <h1>{this.props.title}</h1>
-    )
-  }
+// Create a stateless function component of Title.
+function Title(props) {
+  return (
+    <h1>{props.title}</h1>
+  )
 }
+
+// class Title extends Component {
+//   render() {
+//     return (
+//       <h1>{this.props.title}</h1>
+//     )
+//   }
+// }
 
 export default Title;
