e8a751b45ccbfd21996693d330e1c49b9970fdbe
Hafidz <halfpheez92@gmail.com>
2019-10-04 23:49:51 +0800

    added firebase setup & config.js.

---
+++ photowall/src/database/config.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+// Firebase configuration.
+var firebaseConfig = {
+    apiKey: "AIzaSyAIbvLxW6smmB2lyl0l9lHlJIPrydW_f9Y",
+    authDomain: "photowall-8fce2.firebaseapp.com",
+    databaseURL: "https://photowall-8fce2.firebaseio.com",
+    projectId: "photowall-8fce2",
+    storageBucket: "",
+    messagingSenderId: "1020628674574",
+    appId: "1:1020628674574:web:b04c52ea81608ddff5345f",
+    measurementId: "G-47LLQ9TLJR"
+  };
+
+  // Initialize Firebase.
+  firebase.initializeApp(firebaseConfig);
+
