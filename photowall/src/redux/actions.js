a0ce3656842505e84c4aeb3d006ca36e94da4506
Hafidz <halfpheez92@gmail.com>
2019-10-08 22:52:31 +0800

    updated actions.js with action method to update the database.

--- photowall/src/redux/actions.js
+++ photowall/src/redux/actions.js
@@ -1,5 +1,21 @@
 /// Action creators.
 
+import {database} from '../database/config';
+
+// Update the database first, then dispatch an action creator.
+// Whenever submit a post, update the database first, once it's done updating,
+// It will dispatch an action creator with dispatch method.
+export function startAddingPost(post) {
+    return (dispatch) => {
+
+        // Update the database with promise.
+        // After update the database, it will dispatch an action that updates the Redux store.
+        return database.ref('posts').update({[post.id]: post}).then(() => {
+            dispatch(addPost(post));
+        });
+    }
+}
+
 // Add action remove function.
 // Return a js object that describes the event that should update the application state.
 export function removePost(index) {
