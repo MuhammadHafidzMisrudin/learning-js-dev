573c16dcd0d8d19b39a762eff759d41e4319c91c
Hafidz <halfpheez92@gmail.com>
2020-01-20 23:28:06 +0800

    used react hook. initialised task state variable.

--- react-task-manager/src/context/TaskListContext.js
+++ react-task-manager/src/context/TaskListContext.js
@@ -1,4 +1,19 @@
-import React, {createContext, useState} from 'react';
+import React, { createContext, useState } from 'react';
 
-// initialise and create the context.
+// Initialise and create the context.
 export const TaskListContext = createContext();
+
+const TaskListContextProvider = () => {
+
+    // Use hook. It declares a task “state variable” of an array as initial value.
+    // Use array destructuring to set variable (array of objects) and function.
+    const [tasks, setTasks] = useState([
+        { task: "Text and call Inma", id: 1 },
+        { task: "Write code", id: 2 },
+        { task: "Make dinner for Inma", id: 3 }
+    ]);
+
+    return (<div>Task List Context</div>)
+}
+
+export default TaskListContextProvider; 
