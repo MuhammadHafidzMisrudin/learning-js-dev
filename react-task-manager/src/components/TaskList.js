807a43292902fd6f56df8a559d18fb580f44b94d
Hafidz <halfpheez92@gmail.com>
2020-01-21 00:03:19 +0800

    imported react hooks and context to get access to tasks array.

--- react-task-manager/src/components/TaskList.js
+++ react-task-manager/src/components/TaskList.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {TaskListContext} from "../context/TaskListContext";
 
 // Create a Task List functional component.
 const TaskList = () => {
+
+    // Use useContext hook to call the context.
+    // Use destructuring to get access to tasks array from the state.
+    const {tasks} = useContext(TaskListContext);
+
     return (
         <div>Task List</div>
     );
