0f278fba388068ed57d3ac4ba1685b7213df5278
Hafidz <halfpheez92@gmail.com>
2020-02-18 21:50:05 +0800

    updated app component for styles effect.

--- react-task-manager/src/components/App.js
+++ react-task-manager/src/components/App.js
@@ -7,9 +7,13 @@ const App = () => {
     return (
         // Wrap all components of entire app with the context provider.
         <TaskListContextProvider>
-            <div>
-                {/* Invoke TaskList component */}
-                <TaskList />
+            <div className="container">
+                <div className="app-wrapper">
+                    <div className="main">
+                        {/* Invoke TaskList component */}
+                        <TaskList />
+                    </div>
+                </div>
             </div>
         </TaskListContextProvider>
     );
