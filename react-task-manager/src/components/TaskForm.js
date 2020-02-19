09cb5d9697b8f80004d6a0fb706d4a64e26e9ceb
Hafidz <halfpheez92@gmail.com>
2020-02-19 22:40:34 +0800

    updated task form component with comments.

--- react-task-manager/src/components/TaskForm.js
+++ react-task-manager/src/components/TaskForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 // Create a Task Form functional component.
+// It includes an input field and button function.
+// This is to add the data of tasks into task list and to clear tasks off.
 const TaskForm = () => {
     return (
         <div>
