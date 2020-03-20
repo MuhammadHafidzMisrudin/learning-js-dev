ce8fc6a1d7b13df7ecca0770caf545aee9160217
Hafidz <halfpheez92@gmail.com>
2020-03-21 00:14:48 +0800

    updated task component, invoked removetask method.

--- react-task-manager/src/components/Task.js
+++ react-task-manager/src/components/Task.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useContext} from "react";
+import { TaskListContext } from "../context/TaskListContext";
 
 // Create a Task functional component.
 // To display the list of Task item form.
 const Task = ({task}) => {
     // Use destructuring using task to get access to the task item.
     // Or pass "props" as an argument or parameter. And add "props.task.taskTitle" as {props.task.taskTitle} in the element.
+    
+    // Use useContext hook to call the context.
+    // Use destructuring to get grab access to removeTask function.
+    const { removeTask } = useContext(TaskListContext);
+    
     return (
         <li className="list-item">
             <span>{task.taskTitle}</span>
             <div>
-                <button className="btn-delete task-btn">
+                <button onClick={() => {removeTask(task.id)}} className="btn-delete task-btn">
                     <i className="fas fa-trash-alt"></i>
                 </button>
 
