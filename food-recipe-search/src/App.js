f8152c1bf484733e7d44dcda7e78cfb645280114
Hafidz <halfpheez92@gmail.com>
2020-04-30 23:55:38 +0800

    updated styles & imported styling class.

--- food-recipe-search/src/App.js
+++ food-recipe-search/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
     return (
-        <div>
+        <div className="App">
             Food Recipe Search App
         </div>
     );
