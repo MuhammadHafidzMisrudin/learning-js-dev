99d9738da969b438115101ffee8c36bf9376fba4
Hafidz <halfpheez92@gmail.com>
2020-05-04 23:04:16 +0800

    added recipe search api url for api request.

--- food-recipe-search/src/App.js
+++ food-recipe-search/src/App.js
@@ -2,6 +2,17 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
+
+    // application id.
+    const APP_ID = "ba5830c4";
+
+    // application keys.
+    const APP_KEY = "77504f161b323b537d85da25b0e8f9ad";
+
+    // url search recipe api.
+    // eslint-disable-next-line
+    const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
     return (
         <div className="App">
             <h1>Food Recipe Search Engine</h1>
