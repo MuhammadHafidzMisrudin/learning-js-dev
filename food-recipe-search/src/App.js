feed93c384b056aee3fa2d1574f2768a8f2ad33f
Hafidz <halfpheez92@gmail.com>
2020-05-11 17:26:34 +0800

    implemented onchange function to change input query dynamically.

--- food-recipe-search/src/App.js
+++ food-recipe-search/src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 
 const App = () => {
 
-    // initialise a state (query) using hook.
+    // initialise a state (query) initially using hook.
     // a query to be the data that should be updated.
-    // setQuery should be the method to update the data.
+    // setQuery should be the method to update the data state.
     const [query, setQuery] = useState("");
 
     // application id.
@@ -16,7 +16,7 @@ const App = () => {
     const APP_KEY = "77504f161b323b537d85da25b0e8f9ad";
 
     // url search recipe api.
-    const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
     // get data from the request via search api call.
     const getData = async () => {
@@ -31,6 +31,14 @@ const App = () => {
         getData();
     };
 
+    // an onChange event handler returns a Synthetic Event object which contains useful meta data 
+    // such as the target input’s id, name, and current value.
+    // to access the target input’s value => e.target.value.
+    const onChange = (e) => {
+        console.log(e.target.value);
+        setQuery(e.target.value); // to update the query state dynamically.
+    };
+
     return (
         <div className="App">
             {/* heading */}
@@ -38,7 +46,7 @@ const App = () => {
 
             {/* input form */}
             <form className="search-form" action="" onSubmit={onSubmit}>
-                <input type="text" placeholder="Search Food" autoComplete="off" />
+                <input type="text" placeholder="Search Food" autoComplete="off" onChange={onChange} />
                 <input type="Submit" defaultValue="Search" />
             </form>
         </div>
