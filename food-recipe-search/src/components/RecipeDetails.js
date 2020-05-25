cd21366a4429cb1e2950274c54df445cd119229e
Hafidz <halfpheez92@gmail.com>
2020-05-25 23:38:30 +0800

    added comment in the component.

--- food-recipe-search/src/components/RecipeDetails.js
+++ food-recipe-search/src/components/RecipeDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// functional component to display the details of the Recipe data.
 const RecipeDetails = () => {
     return (<div>Recipe Details Component</div>);
 };
