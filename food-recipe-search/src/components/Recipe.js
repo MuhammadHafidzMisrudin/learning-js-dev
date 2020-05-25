675ca012f6864e8a75b217f929052b17dc605a4e
Hafidz <halfpheez92@gmail.com>
2020-05-25 23:48:28 +0800

    added and invoked recipe details component.

--- food-recipe-search/src/components/Recipe.js
+++ food-recipe-search/src/components/Recipe.js
@@ -1,4 +1,5 @@
 import React from "react";
+import RecipeDetails from "./RecipeDetails";
 
 // functional component to display a Recipe data from request.
 const Recipe = ({recipe}) => {
@@ -13,6 +14,7 @@ const Recipe = ({recipe}) => {
             <img src={image} alt={label} />
             <a href={url} target="_blank" rel="noopener noreferrer">URL link!</a>
             <button>Ingredients</button>
+            <RecipeDetails />
         </div>
     );
 };
