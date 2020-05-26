a2c2ddc9557b860e75b0a205003f848f5aafd7e6
Hafidz <halfpheez92@gmail.com>
2020-05-26 23:56:06 +0800

    added comments of authoring styles for recipe component.

--- food-recipe-search/src/components/Recipe.js
+++ food-recipe-search/src/components/Recipe.js
@@ -1,6 +1,13 @@
 import React from "react";
 import RecipeDetails from "./RecipeDetails";
 
+/* 
+    Author: Muhammad Hafidz Misrudin
+    Description: Proprietary (no permission for copy or use or redistribution)
+    Date written: 20/05/2020
+    Date last updated: 25/05/2020
+*/
+
 // functional component to display a Recipe data from request.
 const Recipe = ({recipe}) => {
 
