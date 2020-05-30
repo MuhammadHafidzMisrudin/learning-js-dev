2fc518f24cc4da8da6147532a32f821b8a3761f1
Hafidz <halfpheez92@gmail.com>
2020-05-31 00:34:46 +0800

    updated recipe component with button to toggle recipe details.

--- food-recipe-search/src/components/Recipe.js
+++ food-recipe-search/src/components/Recipe.js
@@ -5,7 +5,7 @@ import RecipeDetails from "./RecipeDetails";
     Author: Muhammad Hafidz Misrudin
     Description: Proprietary (no permission for copy or use or redistribution)
     Date written: 20/05/2020
-    Date last updated: 25/05/2020
+    Date last updated: 31/05/2020
 */
 
 // functional component to display a Recipe data from request.
@@ -23,10 +23,11 @@ const Recipe = ({recipe}) => {
             <h2>{label}</h2>
             <img src={image} alt={label} />
             <a href={url} target="_blank" rel="noopener noreferrer">URL link!</a>
-            <button>Ingredients</button>
+            <button onClick={() => setShow(!show)}>Ingredients</button>
 
             {/* invoke Recipe Details component. */}
-            <RecipeDetails ingredients={ingredients} />
+            {/* check if show status is true then display recipe data. */}
+            {show === true && <RecipeDetails ingredients={ingredients} />}
         </div>
     );
 };
