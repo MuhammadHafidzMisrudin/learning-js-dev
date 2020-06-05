8463f9161c9c49456f86782c231f844551802ff5
Hafidz <halfpheez92@gmail.com>
2020-06-05 22:38:49 +0800

    updated date updated author comment.

--- food-recipe-search/src/App.js
+++ food-recipe-search/src/App.js
@@ -9,7 +9,7 @@ import Alert from "./components/Alert";
     Author: Muhammad Hafidz Misrudin
     Description: Proprietary (no permission for copy or use or redistribution)
     Date written: 04/05/2020
-    Date last updated: 02/06/2020
+    Date last updated: 04/06/2020
 */
 
 const App = () => {
